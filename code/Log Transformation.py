7318ff9e48e08098ae7e53358be8d8be41cc527b
Pritam Das <40018547+ImDarkShadow@users.noreply.github.com>
2023-03-01 21:21:29 +0530

    power Transformation

--- code/Log Transformation.py
+++ code/Log Transformation.py
@@ -1,12 +1,9 @@
 import numpy as np
 import sys
 
-# Read input image from command line argument
-if len(sys.argv) < 2:
-    print("Please provide the input image as a command line argument")
-    sys.exit()
-    
+
 input_file = sys.argv[1]
+output_file = sys.argv[2]
 input_image = np.loadtxt(input_file, skiprows=3)
 
 # Apply log transformation
@@ -16,8 +13,6 @@ log_transformed_image = c * np.log(1 + input_image)
 # Normalize pixel values to 0-255 range
 log_transformed_image = log_transformed_image / np.max(log_transformed_image) * 255
 
-# Write log-transformed image to file
-output_file = input_file[:-4] + "_log_transformed.pgm"
 with open(output_file, "w") as f:
     # Write header
     f.write("P2\n")
@@ -30,6 +25,4 @@ with open(output_file, "w") as f:
         for y in range(log_transformed_image.shape[1]):
             f.write(str(int(log_transformed_image[x][y])) + " ")
         f.write("\n")
-
-print("Log-transformed image written to {}".format(output_file))
 
